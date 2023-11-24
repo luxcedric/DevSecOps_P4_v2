c5a5aa006c71a4a244520a544e6d3ed3c16c0415
luxcedric <98240622+luxcedric@users.noreply.github.com>
2023-11-24 13:47:25 +0100

    Update vulnerable.php

--- PHPService2/basicxss/vulnerable.php
+++ PHPService2/basicxss/vulnerable.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../template/head.php');
 echo('Sorry, we could not find any results for: ');
-echo($_GET['q']);
+echo(htmlspecialchars($_GET['q']));
 require_once('../template/foot.php');
 ?>
